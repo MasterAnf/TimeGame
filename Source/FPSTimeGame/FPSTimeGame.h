7958489f944b6110d7dad512f1d2c3ba929c52a8
AIE\s171172 <emarsland1214@outlook.com>
2017-07-28 14:48:04 +1000

    Initial commit

---
+++ Source/FPSTimeGame/FPSTimeGame.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __FPSTIMEGAME_H__
+#define __FPSTIMEGAME_H__
+
+#include "EngineMinimal.h"
+
+
+#endif
---
+++ Source/FPSTimeGame/TimeLord.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSTimeGame.h"
+#include "TimeLord.h"
+
+
+
+
